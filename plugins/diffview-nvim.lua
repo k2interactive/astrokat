76807a45281e18b16d5826eba7712e58cdb46ca0
k2interactive <k2interactivestudios@gmail.com>
2023-12-01 07:31:40 -0800

    fix: window resizing

--- plugins/diffview-nvim.lua
+++ plugins/diffview-nvim.lua
@@ -7,6 +7,19 @@ return {
   {
     "NeogitOrg/neogit",
     optional = true,
-    opts = { integrations = { diffview = true } },
+    opts = {
+      file_panel = {
+        listing_style = "tree", -- One of 'list' or 'tree'
+        tree_options = { -- Only applies when listing_style is 'tree'
+          flatten_dirs = true, -- Flatten dirs that only contain one single dir
+          folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+        },
+        win_config = { -- See ':h diffview-config-win_config'
+          position = "left",
+          width = 35,
+          win_opts = {},
+        },
+      },
+    },
   },
 }
