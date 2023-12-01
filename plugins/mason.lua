9cb13166ee83579992b67cab13deb608433742b4
k2interactive <k2interactivestudios@gmail.com>
2023-12-01 07:32:04 -0800

    feat: add python to mason and null-ls

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -9,6 +9,7 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "eslint",
         "lua_ls",
+        "pyright",
         "tsserver",
       })
 
@@ -33,6 +34,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "mypy",
         "prettier",
         "stylua",
       })
