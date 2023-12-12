924896a8d5d90ed01ee1961b8a9b33c9ef19b664
k2interactive <k2interactivestudios@gmail.com>
2023-12-11 20:59:50 -0800

    mod: update colors

--- highlights/duskfox.lua
+++ highlights/duskfox.lua
@@ -9,6 +9,7 @@ return { -- a table of overrides/changes to the duskfox theme
   HopNextKey = { fg = "#00ff00" },
   HopNextKey1 = { fg = "#00ff00" },
   HopNextKey2 = { fg = "#00ff00" },
+  Visual = { fg = "#dddddd", bg = "#004e53" },
   DiffAdd = { fg = "#3344ff", bg = "#000000" }, -- diffview-nvim - <<<<<<, HEAD, and simalar
   DiffDelete = { fg = "#ee0000", bg = "#000000" }, -- diffview-nvim - Deleted Chars
   DiffChange = { fg = "#00ff00", bg = "#000000" }, -- diffview-nvim - Changed chars
@@ -20,6 +21,9 @@ return { -- a table of overrides/changes to the duskfox theme
   -- DiagnosticSignInactive = { fg = "#504550" },
   DiagnosticSignInactive = { fg = "#000000" },
   VertSplit = { fg = "#ffffff", bg = "#000000" },
+
+  HighlightUndo = { fg = "#dddddd", bg = "#ee0000" },
+  HighlightRedo = { fg = "#dddddd", bg = "#0000ee" },
 
   StatuslineDarkText = { fg = "black" },
   StatuslineLightText = { fg = "#605560" },
@@ -35,4 +39,12 @@ return { -- a table of overrides/changes to the duskfox theme
   TabLine = { bg = "#64449a", fg = "#111111" },
   TabLineFill = { bg = get_hl("Keyword").bg, fg = "#7454aa" },
   MoreMsg = { fg = get_hl("Keyword").fg },
+
+  -- ["LspReferenceText"] = { bg = "#0000ff" },
+  -- ["LspReferenceRead"] = { bg = "#ff00ff" },
+  -- ["LspReferenceWrite"] = { bg = "#0ff0ff" },
+
+  LspReferenceRead = { underline = true },
+  LspReferenceWrite = { underline = true },
+  LspReferenceText = { underline = true },
 }
