6b7cd176efdf2c08f2c3bf347c2077b56c17219a
k2interactive <k2interactivestudios@gmail.com>
2023-12-14 22:20:35 -0800

    mod: improve highlight-undo

--- plugins/highlight-undo.lua
+++ plugins/highlight-undo.lua
@@ -1,10 +1,23 @@
 return {
-  "tzachar/highlight-undo.nvim",
-  opts = {
-    duration = 500,
-    redo = {
-      lhs = "<C-u>",
+  {
+    "tzachar/highlight-undo.nvim",
+    event = "VeryLazy",
+    opts = {
+      duration = 500,
+      undo = {
+        hlgroup = "HighlightUndo",
+        mode = "n",
+        lhs = "u",
+        map = "undo",
+        opts = {},
+      },
+      redo = {
+        hlgroup = "HighlightRedo",
+        mode = "n",
+        lhs = "<A-u>",
+        map = "redo",
+        opts = {},
+      },
     },
   },
-  lazy = false,
 }
